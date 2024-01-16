22c2d22fa2a45e341d3dafd5512926abbeb9dc44
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-01-17 00:23:47 +0900

    update

--- lib/essential/join.rb
+++ lib/essential/join.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-# Essential
+# The Essential module contains essential functionalities.
 module Essential
-  # Essential::Join
+  # The Join module provides methods for joining data structures.
   module Join
     class << self
       # Performs a hash join operation on two hashes.
+      #
       # Given two hashes, this method iterates through the keys of the first hash and checks
       # if the second hash contains a corresponding key. If a match is found, the values
       # associated with the matching keys are combined into an array. The method returns a
@@ -21,7 +22,7 @@ module Essential
       # @example
       #   hash1 = { :a => 1, :b => 2, :c => 3 }
       #   hash2 = { :b => 4, :c => 5, :d => 6 }
-      #   result = hash_join(hash1, hash2)
+      #   result = Essential::Join.hash_join(hash1, hash2)
       #   # => [{:a=>1, :b=>[2, 4], :c=>[3, 5]}, [:d]]
       #
       # @note The original hashes remain unchanged.
@@ -30,6 +31,32 @@ module Essential
         not_match = hash2.keys - hash1.keys
         joined_hash.reject! { |key| not_match.include?(key) }
         [joined_hash, not_match]
+      end
+
+      # Generates combinations of arguments and creates files based on the combinations.
+      #
+      # @param args1 [Array] The first array of arguments.
+      # @param args2 [Array] Additional arrays of arguments.
+      #
+      # @yield [Array] The block should return the contents for each combination.
+      #
+      # @example
+      #   args1 = [:a, :b]
+      #   args2 = [1, 2]
+      #   Essential::Join.generate_combinations(args1, args2) do |combination|
+      #     "Content for #{combination}"
+      #   end
+      def generate_combinations(args1, *args2)
+        Dir.mkdir(Essential.result, 0o755) unless File.directory?(Essential.result)
+
+        args1.product(*args2).each do |combination|
+          file_name = combination.join('_')
+          full_path = File.join(Essential.result, file_name)
+          contents = yield(combination)
+          next if contents.nil?
+
+          File.write(full_path, contents.to_s)
+        end
       end
     end
   end
