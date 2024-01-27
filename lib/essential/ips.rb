b514c8694050a8ad1791530bae838f296ca1de08
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-01-27 19:23:29 +0900

    update

--- lib/essential/ips.rb
+++ lib/essential/ips.rb
@@ -33,19 +33,9 @@ module Essential
       # @return [IPAddr] The IPAddr object representing the IP address.
       def line_to_ip(line)
         # Formats the line, eliminating extra spaces and editing the format for IP addresses.
-        line = eliminate_extra_spaces(line)
-        line = edit_format_for_ipaddr(line)
-        IPAddr.new(line)
-      end
-
-      # Get the prefix of an IPAddr object.
-      #
-      # @param ip_target_object [IPAddr] The IPAddr object.
-      # @return [String] The prefix of the IP address.
-      def get_prefix(ip_target_object)
-        # Extracts the prefix from the IPAddr object.
-        ip_target = ip_target_object.inspect.gsub(%r{.*/}, '')
-        ip_target.gsub(/>$/, '')
+        temp_line = eliminate_extra_spaces(line)
+        temp_line = edit_format_for_ipaddr(temp_line)
+        IPAddr.new(temp_line)
       end
 
       # Convert a line to an array of IP addresses.
